e531ea244a74dc3b8458de4bf12d8632225238f0
Joe Ipson <joe@ipson.me>
2020-12-12 00:07:18 -0700

    Whoops

--- src/background.js
+++ src/background.js
@@ -41,10 +41,6 @@ function createWindow() {
     },
   });
 
-  if (isWindows) {
-    win.setThumbarButtons([]);
-  }
-
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
